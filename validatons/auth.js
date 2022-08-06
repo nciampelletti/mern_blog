4fec71db304633f621371c3f20f3d7be33cedb27
nciampelletti <natashat@shaw.ca>
2022-08-05 23:42:34 -0700

    work on regiter route

--- validatons/auth.js
+++ validatons/auth.js
@@ -1,1 +1,8 @@
 import { body } from "express-validator"
+
+export const registerValidator = [
+  body("email", "Invalid email format").isEmail(),
+  body("password", "Password must be as min as 5 symbols").isLength({ min: 5 }),
+  body("fullName", "Name is required").isLength({ min: 3 }),
+  body("avatarUrl", "Incorrect url").optional().isURL(),
+]
